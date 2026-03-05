33953bf2d0f0a320709cd991a63883453241cb15
David Stojcic <da.stojcic@gmx.de>
2026-03-05 11:44:46 +0100

    Add Structure of files and beginning with Learning Type Script

---
+++ LearningTypeScript.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Hello World");
+var age = 23;
+age = 'I love TypeScript!';
+console.log(age);
---
+++ LearningTypeScript.ts
@@ -0,0 +1,9 @@
+export {};
+
+console.log("Hello World");
+
+let age: any = 23;
+
+age = 'I love TypeScript!'; 
+
+console.log(age); 
